f2ca7f30347bb5944c390887ee11dafb7025b7a4
teflon23 <107252192+teflon23@users.noreply.github.com>
2025-02-11 00:46:54 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,43 @@
+﻿// ConsoleApplication3.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    while (true) {
+
+
+        int sayi, t1, t2;
+        cout << "bir faktoriyel sayi giriniz";
+        cin >> sayi;
+        t1 = 1;
+        for (int i = 1;i <= sayi;i++) {
+            t1 = t1 * i;
+
+        }
+        cout << "faktoriyel sonucu :" << t1 << endl;
+        cout << "devam etmek istiyormusunuz e/h";
+        char devam;
+        cin >> devam;
+        if (devam == 'h') {
+            break;
+
+
+
+
+        }
+    }
+}
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
